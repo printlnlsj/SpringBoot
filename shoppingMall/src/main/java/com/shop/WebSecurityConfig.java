214711f89dc0035a4446ed1783ce8c5b5e5eca08
LeeSeungJae <printlnlsj@gmail.com>
2023-07-14 16:09:44 +0900

    first commit

---
+++ shoppingMall/src/main/java/com/shop/WebSecurityConfig.java
@@ -0,0 +1,27 @@
+package com.shop;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class WebSecurityConfig {
+	
+	@Bean
+	BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+	
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		
+		http.formLogin().disable()
+			.csrf().disable()
+			.cors().disable();			
+		
+		return http.build();
+	}
+
+}
